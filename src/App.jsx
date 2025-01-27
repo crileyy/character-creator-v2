3b00491e54d033a9d63af3f9e40abb4a178df075
crileyy <criley0007@gmail.com>
2025-01-27 16:14:07 -0500

    Completed exercises 1-4

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,16 @@ import React from 'react';
 import CharacterEditor from './components/CharacterEditor';
 import Footer from './components/Footer';
 
+import './app.css';
+
 function App() {
   return (
     <>
+      <div className="app">
       <CharacterEditor />
       <Footer />
+      </div>
+      <div className="grayBackground"></div>
     </>
   );
 }
